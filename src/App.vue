837debe1c5c47d53ff980b1df2c22fcd63634f2c
mriwa <marwa.rekik@esprit.tn>
2020-03-20 15:54:24 +0100

    state managment fot the stocks

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,10 @@
 export default {
     components: {
         appHeader: Header
+    },
+
+    created() {
+        this.$store.dispatch('initStocks')
     }
 }
 </script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
-   <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+   <app-stock v-for="stock in stocks" 
+   :stock="stock"></app-stock>
 
 </div>
 </template>
@@ -14,14 +15,9 @@ export default {
         appStock: Stock
     },
 
-    data() {
-        return {
-            stocks: [
-                { id: 1, name: "BMW", price: 110},
-                { id: 2, name: "Google", price: 200},
-                { id: 3, name: "Apple", price: 250},
-                { id: 4, name: "Twitter", price: 8}
-            ]
+    computed: {
+        stocks() {
+            return this.$store.getters.stocks;
         }
     }
 }
