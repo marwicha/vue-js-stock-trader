807919ac42e43d7d7816340df0f5ae9787dd287f
mriwa <marwa.rekik@esprit.tn>
2020-03-21 00:27:03 +0100

    vue resource, save and get data with firebase

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,10 +25,13 @@
                                 data-toggle="dropdown"
                                 role="button"
                                 aria-haspopup="true"
-                                aria-expanded="false">Save & Load <span class="caret"></span></a>
+                                aria-expanded="false"
+                                
+                                
+                                >Save & Load <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Save Data</a></li>
-                            <li><a href="#" >Load Data</a></li>
+                            <li><a href="#" @click="saveData">Save Data</a></li>
+                            <li><a href="#" @click="loadData">Load Data</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -59,12 +62,27 @@ export default {
     },
 
     methods: {
-        ...mapActions([
-            'randomizeStocks'
-        ]),
+        ...mapActions({
+            randomizeStocks: 'randomizeStocks',
+            fetchData: 'loadData'
+        }),
 
         endDay() {
          this.randomizeStocks()
+        },
+
+        saveData() {
+            const data = {
+                funds: this.$store.getters.funds,
+                stockPortfolio: this.$store.getters.stockPortfolio,
+                stocks: this.$store.getters.stocks
+            };
+
+            this.$http.put('data.json', data);
+        },
+
+        loadData() {
+            this.fetchData()
         }
     },
 }
--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
 import { routes } from './routes'
 import store from './store/store'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vue-js-stock-trader-87f9d.firebaseio.com/'
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString()
